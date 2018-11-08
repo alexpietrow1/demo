acaba006dd9623b7cda5306202a4470edc10e91a
alexpietrow1 <44782490+alexpietrow1@users.noreply.github.com>
2018-11-08 14:15:32 -0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,11 @@
+from random import randint
+TITLE = "jumpy"
+#screen dims
+WIDTH = 480
+HEIGHT = 600
+#frames per second
+FPS = 30
+#colors
+WHITE = (255, 255, 255)
+BLACK = (randint(0,250), randint(0,255), randint(0,255))
+REDDISH = (randint(0,255), randint(0,255), randint(0,255))
