5a3b382432de5342430ee6684d2e21a74381e4d4
alexpietrow1 <44782490+alexpietrow1@users.noreply.github.com>
2018-12-03 10:02:01 -0800

    Week 16 Code

--- settings.py
+++ settings.py
@@ -1,27 +1,49 @@
-from random import randint
 TITLE = "jumpy"
-#screen dims
+# screen dims
 WIDTH = 480
 HEIGHT = 600
-#player settings
-PLAYER_ACC = 0.75
-PLAYER_FRICTION = -0.12
-PLAYER_GRAV = 0.98
-PLAYER_JUMP = 25
-#starting platforms
-PLATFORM_LIST = [(0, HEIGHT - 10, WIDTH, 10),
-                 (80, HEIGHT - 110, 80, 10),
-                 (240, HEIGHT - 200, 70, 10),
-                 (160, HEIGHT - 310, 75, 10),
-                 (320, HEIGHT - 390, 80, 10),
-                 (80, HEIGHT - 500, 75, 10)]
-
-#frames per second
+# frames per second
 FPS = 60
-#colors
-WHITE = (randint(0,255), randint(0,255), randint(0,255))
-BLACK = (randint(0,255), randint(0,255), randint(0,255))
-BLACK_2 = (0, 0, 0)
-REDDISH = (randint(0,255), randint(0,255), randint(0,255))
-#font
-FONT_NAME = "arial"
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+REDDISH = (240,55,66)
+SKY_BLUE = (143, 185, 252)
+FONT_NAME = 'arial'
+SPRITESHEET = "spritesheet_jumper.png"
+SPRITESHEET2 = "spritesheet.png"
+# data files
+HS_FILE = "highscore.txt"
+# player settings
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+PLAYER_JUMP = 20
+# game settings
+BOOST_POWER = 60
+POW_SPAWN_PCT = 7
+MOB_FREQ = 500
+# layers - uses numerical value in layered sprites
+PLAYER_LAYER = 2
+PLATFORM_LAYER = 1
+POW_LAYER = 1
+MOB_LAYER = 2
+CLOUD_LAYER = 0
+
+# platform settings
+''' old platforms from drawing rectangles'''
+'''
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (65, HEIGHT - 300, WIDTH-400, 40),
+                 (20, HEIGHT - 350, WIDTH-300, 40),
+                 (200, HEIGHT - 150, WIDTH-350, 40),
+                 (200, HEIGHT - 450, WIDTH-350, 40)]
+'''
+PLATFORM_LIST = [(0, HEIGHT-40),
+                (50, HEIGHT-40),
+                (100, HEIGHT-40),
+                (150, HEIGHT-40),
+                 (65, HEIGHT - 300),
+                 (20, HEIGHT - 350),
+                 (200, HEIGHT - 150),
+                 (200, HEIGHT - 450)]
